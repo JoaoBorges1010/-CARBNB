b0c787941e74c4c1805c303be162c9d6a85ef0c0
joaoborges <joaoborgesbiz@gmail.com>
2023-03-03 16:19:18 +0000

    js kinda done

--- app/javascript/controllers/booked_date_controller.js
+++ app/javascript/controllers/booked_date_controller.js
@@ -3,16 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="booked-date"
 export default class extends Controller {
   static targets = ['notAvailable']
-  static values = {"unavailable": Boolean}
+  static values = {"unavailable": Boolean, "endsAt": String}
   connect() {
-    console.log(this.notAvailableTarget)
+    console.log(this.endsAtValue)
 
-    if (this.unavailableValue === false) {
-      this.notAvailabeTarget.classList.add("opacitycard")
-    } else {
-      console.log("TRUE")
-    }
+      if (this.unavailableValue === true ) {
+        this.notAvailableTarget.classList.add("opacitycard")
+      }
 
 
+  }
+
+  finishDate(event) {
+    event.preventDefault()
+    const end = new Date(this.endsAtValue)
+    alert(end)
   }
 }
